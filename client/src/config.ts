000976689d23d7297f334726f574dcf43e2a6cd1
Yusuf Kolawole <quadyusuf@gmail.com>
2022-09-04 19:19:06 +0100

    done

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '63d2xss863';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-gjizm0lb.us.auth0.com',
+  clientId: 'huAzc4FK5OwY5EnWJCub3sNNnrR11fEL',
+  callbackUrl: 'http://localhost:3000/callback'
+};
